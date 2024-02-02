a48edecf7e1e7ffa9ff1476e4b4c33414fbd16f6
ubeoppu <ubeoppu@gmail.com>
2024-02-02 17:58:30 +0900

    test

--- Java_02_02_복습/src/SutdaDeck.java
+++ Java_02_02_복습/src/SutdaDeck.java
@@ -3,26 +3,34 @@ public class SutdaDeck {
 
 	public static void main(String[] args) {
 		
-		boolean isKwang;
-		
+		int j = 1;
 		int[]SutdaCard = new int[20];
 		
 		
-		for(int j = 1; j <= 10;) {
 			for(int i = 0; i <SutdaCard.length; i++) {
-			
-				SutdaCard[i] = j;
-				j++;
-				if(j =)
-			}
-
+				SutdaCard[i] =  j++;
+				if(j >=11) {
+					j = 1;
+				}
 			 
 		}
 
 		
 	
 		for(int i =0; i < SutdaCard.length; i ++) {
-			System.out.println(SutdaCard[i]);				
+			if(SutdaCard[i] != 1 && SutdaCard[i] != 3 && SutdaCard[i] != 8)
+			System.out.print("["+SutdaCard[i] + "]");	
+			
+			else if(i <= 9) {
+			System.out.print("[" + SutdaCard[i] + " 광]");
+			}
+			else if(i >=10){
+			System.out.print("[" + SutdaCard[i] + "]");
 		}
+			System.out.print(" , ");
+			
+			}
+		}					
+		
 	}
-}
+
